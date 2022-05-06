69920bbe5386e011fb2a05c3ffcd43d2fac531fa
Mastersam07 <abadasamuelosp@gmail.com>
2022-05-06 10:21:48 +0100

    :sparkles: ft: implement clickable painter

--- lib/paints/path_painter.dart
+++ lib/paints/path_painter.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:touchable/touchable.dart';
+
+const svgWidth = 1000;
+const svgHeight = 813;
 
 class PathPainter extends CustomPainter {
   final BuildContext context;
   final List<Path> paths;
-  final List<Path> selectedPath;
-  // final Function(Path curPath) onPressed;
+  final Path curPath;
+  final Function(Path curPath) onPressed;
   PathPainter({
     this.context,
     this.paths,
-    // this.onPressed,
-    this.selectedPath = const [],
+    this.curPath,
+    this.onPressed,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
     // calculate the scale factor, use the min value
-    final double xScale = size.width / MediaQuery.of(context).size.width;
-    final double yScale = size.height / MediaQuery.of(context).size.height;
+    final double xScale = size.width / svgWidth;
+    final double yScale = size.height / svgHeight;
     final double scale = xScale < yScale ? xScale : yScale;
 
     // scale each path to match canvas size
@@ -24,11 +28,13 @@ class PathPainter extends CustomPainter {
     matrix4.scale(scale, scale);
 
     // calculate the scaled svg image width and height in order to get right offset
-    double scaledSvgWidth = MediaQuery.of(context).size.width * scale;
-    double scaledSvgHeight = MediaQuery.of(context).size.height * scale;
+    double scaledSvgWidth = svgWidth * scale;
+    double scaledSvgHeight = svgHeight * scale;
     // calculate offset to center the svg image
     double offsetX = (size.width - scaledSvgWidth) / 2;
     double offsetY = (size.height - scaledSvgHeight) / 2;
+
+    final TouchyCanvas touchCanvas = TouchyCanvas(context, canvas);
 
     // your paint
     final Paint paint = Paint()
@@ -37,15 +43,18 @@ class PathPainter extends CustomPainter {
       ..strokeWidth = 1.0;
 
     for (var path in paths) {
-      // Here: archive our target, select one province, just change the paint's style to fill
-      paint.style = selectedPath.contains(path)
-          ? PaintingStyle.fill
-          : PaintingStyle.stroke;
+      // Here: archive our target, select one path/state, just change the paint's style to fill
 
-      canvas.drawPath(
-          // scale and offset each path to match the canvas
-          path.transform(matrix4.storage).shift(Offset(offsetX, offsetY)),
-          paint);
+      paint.style = path == curPath ? PaintingStyle.fill : PaintingStyle.stroke;
+
+      touchCanvas.drawPath(
+        path.transform(matrix4.storage).shift(Offset(offsetX, offsetY)),
+        paint,
+        onTapDown: (details) {
+          // notify select change and redraw
+          onPressed(path);
+        },
+      );
     }
   }
 
